2a3d9d7deae49fe7cc6cf74f6a9a18577578547c
Luis Felipe <luisfelipefernandezbetancur@gmail.com>
2026-04-20 22:57:40 -0500

    fix: chown storage to www-data after swagger generate

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,6 +6,7 @@ php artisan route:cache
 php artisan migrate --force
 php artisan db:seed --force
 php artisan l5-swagger:generate
+chown -R www-data:www-data storage
 
 php-fpm -D
 nginx -g "daemon off;"
